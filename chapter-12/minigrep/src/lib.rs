62bf22c1b5da52483d1d0e3e9dc81657feb34776
Austin Kaylor <AustinJKaylor@eaton.com>
2025-01-10 10:34:50 -0500

    Chapter 12 - Finished refactoring section 12.3

---
+++ chapter-12/minigrep/src/lib.rs
@@ -0,0 +1,61 @@
+﻿use std::error::Error;
+use std::fs;
+
+/// A function to run the program
+/// # Arguments
+/// * `config` - A [Config] instance with the query and file path values
+/// # Returns
+/// * <b>Success:</b> The contents of the file
+/// * <b>Error:</b> A type that implements the [Error] trait
+pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
+    // ? returns the error value from the current function for the caller to handle
+    let contents = fs::read_to_string(config.file_path)?;
+
+    println!("With text:\n{contents}");
+
+    Ok(())
+}
+
+/// A struct to hold the configuration values passed in from the command line
+pub struct Config {
+    /// The query to search for
+    pub query: String,
+    /// The file path to search
+    pub file_path: String,
+}
+
+impl Config {
+    /// Create a new [Config] instance from a string slice
+    /// # Arguments
+    /// * `args` - A slice of the command line arguments
+    /// # Returns
+    /// * <b>Success:</b> A [Config] instance with the query and file path values
+    /// * <b>Error:</b> An error message if the slice is too short
+    pub fn build(args: &[String]) -> Result<Config, &'static str> {
+        if args.len() < 3 {
+            // Err is a generic enum type that represents an error
+            // The &'static str type is a string slice that has the 'static lifetime
+            // Error messages are usually string slices with static lifetimes 
+            //      because they’re baked into the program and always available
+            return Err("Not enough arguments");
+        }
+        /*
+           We used to return string slices that reference String values in args. 
+           We now define Config to contain owned String values. 
+           The args variable in main is the owner of the argument values. 
+           The args variable is only letting the parse_config function borrow them. 
+           That means we’d violate Rust’s borrowing rules if Config tried to take ownership of the values in args
+            
+            Using clone() makes a full copy of the data for the Config instance to own
+            The trade-off for the extra runtime cost is that we get a clear separation of concerns between main and parse_config
+            If we didn't use clone(), we'd have to manage lifetimes of the borrowed references between main and parse_config
+            */
+        let query = args[1].clone();
+        let file_path = args[2].clone();
+
+        Ok(Config {
+            query, // using shorthand initialization. really reads query: query
+            file_path // using shorthand initialization. really reads file_path: file_path
+        })
+    }
+}
--- chapter-12/minigrep/src/main.rs
+++ chapter-12/minigrep/src/main.rs
@@ -1,3 +1,47 @@
+use std::{env, fs, process};
+use minigrep::Config;
+
 fn main() {
-    println!("Hello, world!");
+    /*
+        std::env::args will panic if any argument contains invalid Unicode
+        If your program needs to accept arguments containing invalid Unicode, use std::env::args_os instead
+        That function returns an iterator that produces OsString values instead of String values
+     */
+    // .collect() is a method that takes the iterator and collects the resulting values into a collection data type
+    let args: Vec<String> = env::args().collect();
+    
+    // dbg! is a macro that prints the value of an expression for debugging purposes
+    /*
+        The first value in the vector is "target/debug/minigrep", which is the name of our binary. 
+        This matches the behavior of the arguments list in C.
+        This lets programs use the name by which they were invoked in their execution
+     */
+    dbg!(&args);
+
+    // https://rust-book.cs.brown.edu/ch12-01-accepting-command-line-arguments.html#saving-the-argument-values-in-variables
+    // https://rust-book.cs.brown.edu/ch12-03-improving-error-handling-and-modularity.html#calling-configbuild-and-handling-errors
+    /*
+        .unwrap_or_else()
+        - is a method that is defined on Result<T, E>.
+        - It takes two closures as arguments.
+        - If the Result is an Ok value, unwrap_or_else returns the inner value.
+        - If the Result is an Err value, unwrap_or_else calls the closure that takes the error value as an argument.
+        - The closure passed to unwrap_or_else in this example calls process::exit(1), which exits the program with a status code of 1.
+        - The status code of 1 indicates to the operating system that the program ended with an error.
+        - This allows us to implement custom non-panic! error handling
+     */
+    let config = Config::build(&args).unwrap_or_else(|err| {
+        println!("Problem parsing arguments: {err}");
+        process::exit(1);
+    });
+
+    println!("Searching for {}", config.query);
+    println!("In file {}", config.file_path);
+
+    // https://rust-book.cs.brown.edu/ch12-03-improving-error-handling-and-modularity.html#handling-errors-returned-from-run-in-main
+    // use if let rather than unwrap_or_else to check whether run returns an Err value and to call process::exit(1) if it does
+    if let Err(e) = minigrep::run(config) {
+        println!("Application error: {e}");
+        process::exit(1);
+    }
 }
