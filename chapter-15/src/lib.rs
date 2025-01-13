cd50a92dd322d93a86d727691cac5ec9a65f42d5
Austin Kaylor <AustinJKaylor@eaton.com>
2025-01-13 17:13:23 -0500

    Chapter 15 - Setting up each module for the chapter in lib.rs

--- chapter-15/src/lib.rs
+++ chapter-15/src/lib.rs
@@ -41,12 +41,50 @@
 //! - This pattern uses smart pointers to achieve this.
 //! - An immutable type exposes an API for mutating the interior value
 
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-       
-    }
+/// Module 15.1 - Using Box<T> to Point to Data on the Heap
+/// # See
+/// - [Box<T>](https://doc.rust-lang.org/std/boxed/struct.Box.html)
+/// - [Rust Book - Chapter 15.1](https://doc.rust-lang.org/book/ch15-01-box.html)
+mod box_pointer {
+    
 }
+
+/// Module 15.2 - Treating Smart Pointers Like Regular References with the Deref Trait
+/// # See
+/// - [Deref Trait](https://doc.rust-lang.org/std/ops/trait.Deref.html)
+/// - [Rust Book - Chapter 15.2](https://doc.rust-lang.org/book/ch15-02-deref.html)
+mod deref_trait {
+    
+}
+
+/// Module 15.3 - Running Code on Cleanup with the Drop Trait
+/// # See
+/// - [Drop Trait](https://doc.rust-lang.org/std/ops/trait.Drop.html)
+/// - [Rust Book - Chapter 15.3](https://doc.rust-lang.org/book/ch15-03-drop.html)
+mod drop_trait {
+    
+}
+
+/// Module 15.4 - Rc<T>, the Reference Counted Smart Pointer
+/// # See
+/// - [Rc<T>](https://doc.rust-lang.org/std/rc/struct.Rc.html)
+/// - [Rust Book - Chapter 15.4](https://doc.rust-lang.org/book/ch15-04-rc.html)
+mod rc_pointer {
+    
+}
+
+/// Module 15.5 - RefCell<T> and the Interior Mutability Pattern
+/// # See
+/// - [RefCell<T>](https://doc.rust-lang.org/std/cell/struct.RefCell.html)
+/// - [Rust Book - Chapter 15.5](https://doc.rust-lang.org/book/ch15-05-interior-mutability.html)
+mod refcell {
+    
+}
+
+/// Module 15.6 - Reference Cycles Can Leak Memory
+/// # See
+/// - [Rust Book - Chapter 15.6](https://doc.rust-lang.org/book/ch15-06-reference-cycles.html)
+mod reference_cycles {
+    
+}
+
