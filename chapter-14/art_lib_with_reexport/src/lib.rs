12daee001a40ef15ede4cf6af7e81e34c9484c44
Austin Kaylor <AustinJKaylor@eaton.com>
2025-01-13 15:06:45 -0500

    Chapter 14 - adding re-export version of art_lib

--- chapter-14/art_lib_with_reexport/src/lib.rs
+++ chapter-14/art_lib_with_reexport/src/lib.rs
@@ -4,6 +4,13 @@
 //!
 //! Based on the [Rust Brown Book - Chapter 14.2: Exporting a Convenient Public API with pub use](https://rust-book.cs.brown.edu/ch14-02-publishing-to-crates-io.html#exporting-a-convenient-public-api-with-pub-use)
 
+// Re-export the kinds and utils modules.
+// Makes as better API for the users of the art library.
+// https://rust-book.cs.brown.edu/ch14-02-publishing-to-crates-io.html#exporting-a-convenient-public-api-with-pub-use
+pub use self::kinds::PrimaryColor;
+pub use self::kinds::SecondaryColor;
+pub use self::utils::mix;
+
 /// The kinds module provides types of RYB colors according to the RYB color model.
 pub mod kinds {
     #[derive(Debug, PartialEq, Clone, Copy)]
@@ -52,5 +59,62 @@ pub mod utils {
                 }
             }
         }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::kinds::PrimaryColor;
+    use crate::utils::mix;
+
+    /// Tests the [`mix`] function with the [`PrimaryColor::Red`] and [`PrimaryColor::Yellow`] colors.
+    #[test]
+    fn red_and_yellow_make_orange() {
+        let red = PrimaryColor::Red;
+        let yellow = PrimaryColor::Yellow;
+        let result = mix(red, yellow);
+        assert_eq!(result, Some(crate::kinds::SecondaryColor::Orange));
+    }
+
+    /// Tests the [`mix`] function with the [`PrimaryColor::Red`] and [`PrimaryColor::Blue`] colors.
+    #[test]
+    fn red_and_blue_make_purple() {
+        let red = PrimaryColor::Red;
+        let blue = PrimaryColor::Blue;
+        let result = mix(red, blue);
+        assert_eq!(result, Some(crate::kinds::SecondaryColor::Purple));
+    }
+
+    /// Tests the [`mix`] function with the [`PrimaryColor::Yellow`] and [`PrimaryColor::Blue`] colors.
+    #[test]
+    fn yellow_and_blue_make_green() {
+        let yellow = PrimaryColor::Yellow;
+        let blue = PrimaryColor::Blue;
+        let result = mix(yellow, blue);
+        assert_eq!(result, Some(crate::kinds::SecondaryColor::Green));
+    }
+
+    /// Tests the [`mix`] function with the [`PrimaryColor::Red`] and [`PrimaryColor::Red`] colors.
+    #[test]
+    fn red_and_red_make_none() {
+        let red = PrimaryColor::Red;
+        let result = mix(red, red);
+        assert_eq!(result, None);
+    }
+
+    /// Tests the [`mix`] function with the [`PrimaryColor::Yellow`] and [`PrimaryColor::Yellow`] colors.
+    #[test]
+    fn yellow_and_yellow_make_none() {
+        let yellow = PrimaryColor::Yellow;
+        let result = mix(yellow, yellow);
+        assert_eq!(result, None);
+    }
+
+    /// Tests the [`mix`] function with the [`PrimaryColor::Blue`] and [`PrimaryColor::Blue`] colors.
+    #[test]
+    fn blue_and_blue_make_none() {
+        let blue = PrimaryColor::Blue;
+        let result = mix(blue, blue);
+        assert_eq!(result, None);
     }
 }
