95a0774f02acd8b154951d0918ebc2b42b1f4c35
Austin Kaylor <AustinJKaylor@eaton.com>
2025-01-13 14:56:58 -0500

    Chapter 14 - adding unit tests to art_lib

--- chapter-14/art_lib/src/lib.rs
+++ chapter-14/art_lib/src/lib.rs
@@ -54,3 +54,61 @@ pub mod utils {
         }
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use crate::kinds::PrimaryColor;
+    use crate::utils::mix;
+    use super::*;
+
+    /// Tests the [`mix`] function with the [`PrimaryColor::Red`] and [`PrimaryColor::Yellow`] colors.
+    #[test]
+    fn red_and_yellow_make_orange() {
+        let red = PrimaryColor::Red;
+        let yellow = PrimaryColor::Yellow;
+        let result = mix(red, yellow);
+        assert_eq!(result, Some(crate::kinds::SecondaryColor::Orange));
+    }
+
+    /// Tests the [`mix`] function with the [`PrimaryColor::Red`] and [`PrimaryColor::Blue`] colors.
+    #[test]
+    fn red_and_blue_make_purple() {
+        let red = PrimaryColor::Red;
+        let blue = PrimaryColor::Blue;
+        let result = mix(red, blue);
+        assert_eq!(result, Some(crate::kinds::SecondaryColor::Purple));
+    }
+
+    /// Tests the [`mix`] function with the [`PrimaryColor::Yellow`] and [`PrimaryColor::Blue`] colors.
+    #[test]
+    fn yellow_and_blue_make_green() {
+        let yellow = PrimaryColor::Yellow;
+        let blue = PrimaryColor::Blue;
+        let result = mix(yellow, blue);
+        assert_eq!(result, Some(crate::kinds::SecondaryColor::Green));
+    }
+
+    /// Tests the [`mix`] function with the [`PrimaryColor::Red`] and [`PrimaryColor::Red`] colors.
+    #[test]
+    fn red_and_red_make_none() {
+        let red = PrimaryColor::Red;
+        let result = mix(red, red);
+        assert_eq!(result, None);
+    }
+
+    /// Tests the [`mix`] function with the [`PrimaryColor::Yellow`] and [`PrimaryColor::Yellow`] colors.
+    #[test]
+    fn yellow_and_yellow_make_none() {
+        let yellow = PrimaryColor::Yellow;
+        let result = mix(yellow, yellow);
+        assert_eq!(result, None);
+    }
+
+    /// Tests the [`mix`] function with the [`PrimaryColor::Blue`] and [`PrimaryColor::Blue`] colors.
+    #[test]
+    fn blue_and_blue_make_none() {
+        let blue = PrimaryColor::Blue;
+        let result = mix(blue, blue);
+        assert_eq!(result, None);
+    }
+}
