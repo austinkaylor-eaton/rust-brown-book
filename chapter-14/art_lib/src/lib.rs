4a23b83adcbf55ec9be65dc248c9d7c4d5646b9d
Austin Kaylor <AustinJKaylor@eaton.com>
2025-01-13 14:46:55 -0500

    Chapter 14 - adding integration tests to art_lib

--- chapter-14/art_lib/src/lib.rs
+++ chapter-14/art_lib/src/lib.rs
@@ -6,6 +6,7 @@
 
 /// The kinds module provides types of RYB colors according to the RYB color model.
 pub mod kinds {
+    #[derive(Debug, PartialEq, Clone, Copy)]
     /// The primary colors according to the RYB color model.
     pub enum PrimaryColor {
         Red,
@@ -13,7 +14,8 @@ pub mod kinds {
         Blue,
     }
 
-    /// The secondary colors according to the RYB color model.
+    #[derive(Debug, PartialEq)]
+    /// Colors created by mixing two primary colors in equal amounts according to the RYB color model.
     pub enum SecondaryColor {
         Orange,
         Green,
@@ -25,9 +27,30 @@ pub mod kinds {
 pub mod utils {
     use crate::kinds::*;
 
-    /// Combines two primary colors in equal amounts to create
-    /// a secondary color.
-    pub fn mix(c1: PrimaryColor, c2: PrimaryColor) -> SecondaryColor {
-        // --snip--
+    /// Combines two [`PrimaryColor`]s in equal amounts to create a [`SecondaryColor`].
+    pub fn mix(c1: PrimaryColor, c2: PrimaryColor) -> Option<SecondaryColor> {
+        match c1 {
+            PrimaryColor::Red => {
+                match c2 {
+                    PrimaryColor::Yellow => Some(SecondaryColor::Orange),
+                    PrimaryColor::Blue => Some(SecondaryColor::Purple),
+                    _ => None,
+                }
+            }
+            PrimaryColor::Yellow => {
+                match c2 {
+                    PrimaryColor::Red => Some(SecondaryColor::Orange),
+                    PrimaryColor::Blue => Some(SecondaryColor::Green),
+                    _ => None,
+                }
+            }
+            PrimaryColor::Blue => {
+                match c2 {
+                    PrimaryColor::Red => Some(SecondaryColor::Purple),
+                    PrimaryColor::Yellow => Some(SecondaryColor::Green),
+                    _ => None,
+                }
+            }
+        }
     }
 }
