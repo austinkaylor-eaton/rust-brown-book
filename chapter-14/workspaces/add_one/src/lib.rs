86e4c625f9605b20b038c5bbb2c081d1bd911c3f
Austin Kaylor <AustinJKaylor@eaton.com>
2025-01-13 15:51:09 -0500

    Chapter 14 - workspaces - making the adder binary depend on the add_one library

--- chapter-14/workspaces/add_one/src/lib.rs
+++ chapter-14/workspaces/add_one/src/lib.rs
@@ -1,5 +1,10 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
+/// Adds one to the given number.
+/// # Arguments
+/// * `x`:[i32] - The number to add one to.
+/// # Returns
+/// * [i32] - The result of adding one to [x]
+pub fn add_one(x: i32) -> i32 {
+    x + 1
 }
 
 #[cfg(test)]
@@ -7,8 +12,8 @@ mod tests {
     use super::*;
 
     #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+    fn add_one_where_x_is_2_should_be_3() {
+        let result = add_one(2);
+        assert_eq!(result, 3);
     }
 }
